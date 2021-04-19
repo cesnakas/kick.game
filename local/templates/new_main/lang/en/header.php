8a357d818ea12f3a21e7ce640713fe81344b2a17
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-04-19 16:18:00 +0400

    1.0.5

--- local/templates/new_main/lang/en/header.php
+++ local/templates/new_main/lang/en/header.php
@@ -1,8 +1,13 @@
 <?
-$MESS['NAV_HOME']          = 'Home';
-$MESS['NAV_GAME_SCHEDULE'] = 'Schedule';
+$MESS['NAV_HOME']               = 'Home';
+$MESS['NAV_GAME_SCHEDULE']      = 'Schedule';
+$MESS['NAV_TEAMS']              = 'Rating';
+$MESS['NAV_SUBSCRIPTION_PLANS'] = 'Subscribe';
+$MESS['NAV_REGISTER']           = 'Register';
+$MESS['NAV_LOGIN']              = 'Login';
+$MESS['NAV_PERSONAL']           = 'Profile';
 
-$MESS['NAV_RATINGS']       = 'Ratings';
-$MESS['NAV_SUBSCRIPTION']  = 'Subscription';
-$MESS['NAV_LOGOUT']        = 'Logout';
 $MESS['NAV_LANG']          = 'Рус';
+
+$MESS['MAIN_SUBTITLE'] = 'Your eSports pass, pabger'; // Твой пропуск в киберспорт, пабгер
+$MESS['MAIN_DUO'] = 'Duo tournaments for &euro;100 every week'; // Турниры дуо на 100&euro; каждую неделю
--- local/templates/new_main/lang/ru/header.php
+++ local/templates/new_main/lang/ru/header.php
@@ -1,8 +1,13 @@
 <?
-$MESS['NAV_HOME'] = 'Главная';
-$MESS['NAV_GAME_SCHEDULE'] = 'Расписание';
+$MESS['NAV_HOME']               = 'Главная';
+$MESS['NAV_GAME_SCHEDULE']      = 'Расписание';
+$MESS['NAV_TEAMS']              = 'Рейтинг';
+$MESS['NAV_SUBSCRIPTION_PLANS'] = 'Подписка';
+$MESS['NAV_REGISTER']           = 'Регистрация';
+$MESS['NAV_LOGIN']              = 'Войти';
+$MESS['NAV_PERSONAL']           = 'Профиль';
 
-$MESS['NAV_RATINGS'] = 'Рейтинги';
-$MESS['NAV_SUBSCRIPTION'] = 'Подписка';
-$MESS['NAV_LOGOUT'] = 'Выход';
 $MESS['NAV_LANG'] = 'Eng';
+
+$MESS['MAIN_SUBTITLE'] = 'Твой пропуск в киберспорт, пабгер';
+$MESS['MAIN_DUO'] = 'Турниры дуо на 100&euro; каждую неделю';
