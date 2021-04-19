ceb4de98f5f77c68da828a438e9b8e2e71cbaece
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-04-19 19:39:34 +0400

    1.0.7

--- local/templates/new_main/header.php
+++ local/templates/new_main/header.php
@@ -1,8 +1,6 @@
 <?
 if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) die();
-
 use Bitrix\Main\Page\Asset;
-
 //dump(CUser::IsAuthorized());
 //if(!CUser::IsAuthorized()) {
 //LocalRedirect("/");
@@ -118,26 +116,36 @@ $teamID = $arUser['UF_ID_TEAM'];
                     <svg width="10" height="10" fill="currentColor">
                         <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/img/icons.svg#lang-selector"/>
                     </svg>
-                    <span class="lang-selector__label js-lang-label">ENG</span>
+                    <? if (LANGUAGE_ID == 'ru'): ?>
+                        <span class="lang-selector__label js-lang-label">ENG</span>
+                        <svg width="13" height="13" fill="currentColor">
+                            <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/img/icons.svg#flag-eng"/>
+                        </svg>
+                    <? else: ?>
+                        <span class="lang-selector__label js-lang-label">РУС</span>
+                        <svg width="13" height="13" fill="currentColor">
+                            <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/img/icons.svg#flag-rus"/>
+                        </svg>
+                    <? endif; ?>
+                    <!--<span class="lang-selector__label js-lang-label">ENG</span>
                     <svg width="13" height="13" fill="currentColor">
-                        <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/img/icons.svg#flag-eng"/>
-                    </svg>
-                    <!--//-->
+                        <use xlink:href="<?/*=SITE_TEMPLATE_PATH;*/?>/img/icons.svg#flag-eng"/>
+                    </svg>-->
+
                     <div class="lang-selector__menu">
-                        <a class="lang-selector__menu-item lang-selector__menu-item--active" href="/">
+                        <a class="lang-selector__menu-item <?=(LANGUAGE_ID == 'ru') ? 'lang-selector__menu-item--active' : '';?>" href="/">
                             РУС
-                            <svg width="13" height="13">
-                                <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/img/icons.svg#flag-rus"/>
-                            </svg>
+                            <!--<svg width="13" height="13">
+                                <use xlink:href="<?/*=SITE_TEMPLATE_PATH;*/?>/img/icons.svg#flag-rus"/>
+                            </svg>-->
                         </a>
-                        <a class="lang-selector__menu-item" href="/en/">
+                        <a class="lang-selector__menu-item <?=(LANGUAGE_ID == 'en') ? 'lang-selector__menu-item--active' : '';?>" href="/en/">
                             ENG
-                            <svg width="13" height="13">
-                                <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/img/icons.svg#flag-eng"/>
-                            </svg>
+                            <!--<svg width="13" height="13">
+                                <use xlink:href="<?/*=SITE_TEMPLATE_PATH;*/?>/img/icons.svg#flag-eng"/>
+                            </svg>-->
                         </a>
                     </div>
-                    <!--//-->
                 </div>
 
                 <? if (!$USER->IsAuthorized()): ?>
@@ -210,12 +218,11 @@ $teamID = $arUser['UF_ID_TEAM'];
                 <?php } else { ?>
                     <a href="/personal/" class="button button"><?=GetMessage('NAV_LOGIN')?></a>
                 <?php } ?>
-                Начни путь к победе сегодня!
+                <?=GetMessage('MAIN_WAY')?>
             </div>
             <div class="cta__scroll">
-                <svg xmlns="http://www.w3.org/2000/svg" width="25" viewBox="0 0 512 512">
-                    <path d="M256 0C156.595 0 75.726 82.14 75.726 183.099v145.807C75.726 429.865 156.595 512 256 512c99.399 0 180.274-81.886 180.274-182.534V183.099C436.274 82.14 355.399 0 256 0zm146.366 329.466c0 81.954-65.656 148.627-146.366 148.627-80.705 0-146.366-66.927-146.366-149.192V183.099c0-82.265 65.661-149.192 146.366-149.192 80.711 0 146.366 66.927 146.366 149.192v146.367z"/>
-                    <path d="M256 140.15c-9.364 0-16.954 7.59-16.954 16.954v59.338c0 9.364 7.59 16.954 16.954 16.954s16.954-7.59 16.954-16.954v-59.338c0-9.364-7.59-16.954-16.954-16.954z"/>
+                <svg width="25" height="25" fill="currentColor">
+                    <use xlink:href="<?=SITE_TEMPLATE_PATH;?>/img/icons.svg#mouse-scroll"/>
                 </svg>
             </div>
         </div>
--- local/templates/new_main/lang/en/header.php
+++ local/templates/new_main/lang/en/header.php
@@ -9,8 +9,9 @@ $MESS['NAV_PERSONAL']           = 'Profile';
 
 $MESS['NAV_LANG']          = 'Рус';
 
-$MESS['MAIN_SUBTITLE'] = 'Your eSports pass, pabger'; // Твой пропуск в киберспорт, пабгер
+$MESS['MAIN_SUBTITLE'] = 'Your eSports pass, PUBGer'; // Твой пропуск в киберспорт, пабгер
 $MESS['MAIN_DUO'] = 'Duo tournaments for €100 every week'; // Турниры дуо на 100€ каждую неделю
 $MESS['MAIN_SQUAD'] = 'Squad tournaments for €1000 every month'; // Турниры для сквадов на 1000€ каждый месяц
 $MESS['MAIN_CUSTOMS'] = 'Custom prizes every week'; // Кастомки с призами каждую неделю
 $MESS['MAIN_FREE'] = '14 days to try all the goodies for free'; // 14 дней, чтобы попробовать все плюшки бесплатно
+$MESS['MAIN_WAY'] = 'Start your way to victory today!'; // Начни путь к победе сегодня!
