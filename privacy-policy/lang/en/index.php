3d3303663f007c057c6cc91992c49b46b94eb568
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-04-23 23:25:28 +0400

    1.0.21

---
+++ privacy-policy/lang/en/index.php
@@ -0,0 +1,294 @@
+<?
+$MESS['PP_HEADLINE'] = 'PRIVACY POLICY';
+$MESS['PP_DATE_UPDATE'] = 'Last updated: "22" March 2021';
+$MESS['PP_VERSION'] = 'Version 1.0.';
+
+$MESS['PP_ITEM_P_01'] = 'At KICK GAME eSports, we believe that your privacy is very important. This Policy describes
+types of personally identifiable information and other data that we will collect from you when
+you use our Website https://kick.game/ ("Website", "Platform"), as well as the use of
+KICK GAME eSports services in accordance with the User Agreement, as well as how we will
+use and when we may disclose such information and data to third parties.';
+$MESS['PP_ITEM_P_02'] = 'By registering on the Platform, you agree to this Privacy Policy and our
+The User Agreement. We display a link to our Privacy Policy at the bottom
+the footer of our website.';
+$MESS['PP_ITEM_P_03'] = 'The data controller in relation to the Platform is KICK GAME SPORTS LIMITED (registration number
+number NOT 416108), registered under the laws of the Republic of Cyprus, address
+Registered office: Vasili Michailidi, 21, Limassol, 3026, Cyprus / Vasili Michailidi, 21,
+Limassol, 3026, Cyprus (hereinafter - "We", "KICK GAME eSports", "Company").';
+$MESS['PP_ITEM_P_04'] = '"You" - A user who has registered a Personal Account and uses the internal functionality of the Website
+("User").';
+$MESS['PP_ITEM_P_05'] = 'If you have any questions about this Privacy Policy, you can contact
+by us at support@kick.game.';
+
+$MESS['PP_LIST_ITEM_01'] = 'DEFINING THE TYPES OF INFORMATION DISCUSSED IN THIS ARTICLE
+POLITICS:';
+$MESS['PP_LIST_ITEM_02'] = '<p> "Personal Information" is a type of information that specifically identifies you personally and/or
+it can be used separately to contact you online or offline, for example, your full name,
+physical address, email address, phone number, photo, accurate information about
+geolocation, map information, and, in some cases, the user name.</p>
+<p> "Non-personal information" is anonymous information about your device, such as your
+the unique device ID or other persistent identifiers, as well as their
+hardware / software / firmware, and usage data
+the Platform that you use, such as the actions that you perform on the Platform that you use
+device, as well as the date and time of your use of the Platform.</p>';
+$MESS['PP_LIST_ITEM_TITLE_02'] = 'WHAT INFORMATION DO WE COLLECT?';
+$MESS['PP_LIST_ITEM_02_1'] = '<p>Personal information</p>
+<p>As a general rule, we do not require or request any Personal Information from you,
+when you use our Platform.</p>
+<p>In the event that we collect Personal Information from you, we will only use it
+to the limited extent necessary to provide you with the services you have requested
+(for example, to send you an email) or to respond to your requests.
+under no circumstances will we ever sell, rent or
+transfer your Personal Information to third parties for direct marketing purposes, for
+monetary or other valuable remuneration.</p>';
+
+$MESS['PP_LIST_ITEM_02_2'] = '<p>Non-personal information</p>
+<p>Below we have listed the types of information we collect. It should not be considered
+as personal information, since we cannot directly or
+indirectly identify you as an individual based on this information. Non-personal information
+it is collected in an anonymous form.</p>
+<p>When you use the Platform, we may collect the following information:</p>
+<ol>
+<ul>
+<li>Platform usage data<br>
+information about how you use the Platform, including your preferred language
+(geography), the date and time when you first started using the Platform, and
+the date and time when you last used the Platform.<br>
+This data will be stored for the entire period of your use of our service.
+Platforms and will be completely removed after 30 days from the date of removal of your
+An account.
+</li>
+<li>
+Purchase history<br>
+if you decide to purchase our Services, then the confirmation that you
+you are a paid subscriber of the Platform.<br>
+This data will be stored for the entire period of your use of our service.
+Platforms and will be completely removed after 30 days from the date of removal of your
+An account.
+
+</li>
+<li>
+Device data<br>
+such as the type and version number of your devices operating system,
+device ID, push tokens, browser type, screen resolution,
+IP address (and the corresponding country where you are located), and other
+information about the device you use to visit
+Platforms.<br>
+This data will be stored for the entire period of your use of our service.
+Platforms and will be completely removed after 30 days from the date of removal of your
+An account.
+</li>
+<li>
+Online activity data<br>
+such as information about your use of the Platform and your actions on it,
+including the pages or screens you viewed, how much time you spent
+on a page or screen, navigation paths between pages or screens,
+information about your actions on the page or screen, access time, and
+duration of access. Our service providers and some third parties
+(for example, advertising networks on the Internet) can also collect this type
+information over time and through third-party websites. We can collect
+this information directly or through the use of third-party kits
+software development ("SDK"), such as Google)). SDKs can
+allow third parties to collect information directly from our
+Platforms.<br>
+This data will be stored for the entire period of your use of our service.
+Platforms and will be completely removed after 30 days from the date of removal of your
+An account.
+</li>
+</ul>
+</ol>';
+
+$MESS['PP_LIST_ITEM_03'] = '<h2 class= "policy-list_title" >HOW DO WE USE NON-PERSONAL INFORMATION?</h2>
+<p>For data collection and analysis, as well as for advertising purposes, we use reliable large
+platforms such as Google, Facebook, Yandex, which collect information on our behalf,
+to:</p>
+<ol><ul>
+<li>Perform a statistical analysis of your use of the Platform (using
+Google Analytics, Yandex Metric and Facebook Analytics).</li>
+<li>To serve you ads (using TikTok Ads, Google Ads, and Facebook Ads).</li>
+</ul></ol>';
+
+$MESS['PP_LIST_ITEM_04'] = '
+<h2 class="policy-list_title">DISCLOSURE OF NON-PERSONAL INFORMATION THAT WE COLLECTING</h2>
+<p>We do not share non-personal information with third parties, unless
+this is necessary for our legitimate business needs, to fulfill your requests, and/or
+required or permitted by applicable law. This includes:</p>
+<ol>
+<li id="4.1">
+<p>SERVICE PROVIDERS:</p>
+<p>We only share information with certain third-party service providers so that they
+can process your information on our behalf to help us work with
+Platform (for example, analytics, marketing) or process payments through services
+these third-party service providers. We only transmit non-personal information that
+allows them to provide us with their services.</p>
+<p>Non-personal information is collected for the following purposes and using
+the following services:</p>
+<h3>Google</h3>
+<p>All non-personal information collected is stored on servers controlled by Google (or its partners).
+subsidiaries).</p>
+<h3>Google Analytics</h3>
+<p>This is an analysis service provided by Google LLC (USA). Google helps us measure
+Platform traffic and understand how people use our Services.</p>
+<p>This integration with Google Analytics anonymizes your IP address.</p>
+<p>For more information about how Google Analytics uses
+For the collected information, please visit Google Privacy Policy:
+https://www.google.com/policies/privacy/</p>
+<p>Type of data processed: Device data; Platform usage data;
+Data about online activity.</p>
+<p>Legal basis for processing: your consent; our legitimate interests-compliance
+our contractual obligations to Google under the Terms of Use Google Analytics and Google Analytics permission to work with our Platform.</p>
+<p>Disclaimer: You can opt out of using Google Analytics by sending us
+email to <a href="mailto:support@kick.game">support@kick.game</a> with a request to disable
+collecting data from Google Analytics servers.</p>
+<h4>Google Eds / Google Ads</h4>
+<p>This is an analysis service provided by Google Ireland Ltd., used to display
+our advertising on third-party services.</p>
+<p>For more information on how Google uses the collected information
+For more information, visit Google Privacy Policy:
+https://www.google.com/policies/privacy/</p>
+<p>Type of data processed: Device data; Platform usage data;
+Data about online activity.</p>
+<p>Legal basis for processing: your consent; our legitimate interests-compliance
+our contractual obligations to Google under the Terms of Use
+Google Advertising.</p>
+<p>Disclaimer: You can opt out of using Google Ads by following the instructions described below
+Google: https://support.google.com/ads/answer/2662922?hl=en. You will still see ads,
+but it will not be based on your interests and info anymore.</p>
+<h3>Yandex. Metrica</h3>
+<p>Yandex is an advertising service provided by Yandex LLC (Russia), which
+helps us measure Platform traffic and understand how people use our services.
+Services.</p>
+<p>For more information about how Yandex. Metrica uses the collected information, see
+Yandex Privacy Policy: https://yandex.ru/legal/confidential/</p>
+<p>Type of data processed: Device data; Platform usage data;
+Data about online activity.</p>
+<p>Legal basis for processing: your consent; our legitimate interests-compliance
+contractual obligations to Yandex in accordance with the Terms of Use
+Yandex.Metrica and AppMetrica services and the permission for Yandex. Metrica to work on our site
+The platform.</p>
+<p>You can opt out of using the Yandex. Metrica service by writing to us at
+<a href="mailto:support@kick.game">support@kick.game</a> with a request to disable the collection
+data from Yandex. Metrica servers.</p>
+<h3>Facebook</h3>
+<p>All non-personal information collected is stored on servers controlled by Facebook (or
+its subsidiaries).</p>
+<h3>Facebook Facebook Analytics Facebook Facebook Analytics is an analytics service provided by Facebook, Inc. (USA) and Facebook Ireland Limited that helps us measure the Platform traffic and understand the Platform traffic flow.</h3>
+<p>Facebook Analytics is an analytics service provided by Facebook, Inc. (USA) and Facebook Ireland Limited.</p>,
+how people use our Services.</p>
+<p>For more information on how Facebook uses the collected information
+For more information, please visit the Privacy Policy page:
+https://www.facebook.com/about/privacy/</p>
+<p>Type of data processed: Device data; Platform usage data;
+Data about online activity.</p>
+<p>Legal basis for processing: your consent; our legitimate interests-compliance
+our contractual obligations to Facebook in accordance with the Terms
+using Facebook Facebook business tools and allowing Facebook analytics to work
+in our App.</p>
+<p>Disclaimer: You can opt out of Facebook analytics by sending us an email at
+address <a href="mailto:support@kick.game">support@kick.game</a> with a request to disable
+collecting data from Facebook analytics servers.</p>
+<h3>Facebook Ads / Facebook Ads </h3>
+<p>It is provided by Facebook Ireland Limited and is used to display our
+ads on third-party services.</p>
+<p>Facebook will collect the data and use your unique ID.
+devices for displaying ads.</p>
+<p>For more information on how Facebook uses the collected information
+For more information, please visit the Privacy Policy page:
+https://www.facebook.com/about/privacy/</p>
+<p>Type of data processed: Device data; Platform usage data;
+data about online activity.</p>
+<p>Legal basis for processing: your consent; our legitimate interests-compliance
+our contractual obligations to Facebook in accordance with the Terms
+using Facebook business tools.</p>
+<p>Disclaimer: You can change your choice for Facebook ads at any time in the settings
+Facebook in the "Settings" section; then the "Ads" tab (you may have to
+log in to your Facebook account). You will still see the ads, but they are bigger
+it will not be based on your interests and information.</p>
+<h3>TikTok</h3>
+<p>All non-personal information collected is stored on servers controlled by TikTok (or
+its subsidiaries).</p>
+<p>TikTok Ads</p>
+<p>It is provided by TikTok, Inc. (USA) and is used to display our ads
+on third-party services.</p>
+</li>
+</ol>';
+
+$MESS['PP_LIST_ITEM_05'] = '<h2 class="policy-list_title">WHEN OUR PRIVACY POLICY DOES NOT APPLY</h2>
+                        <ol>
+                            <li id="5.1">
+                                <p>Third-party links</p>
+                                <p>The Platform may contain links to other websites and other online services,
+managed by third parties. These links do not constitute an endorsement or statement of
+that we are affiliated with any third party. In addition, our content
+may be included on web pages or online services that are not affiliated with us. We
+do not control third-party websites or online services and are not responsible for
+their actions. Other websites and online services follow different rules regarding
+the collection, use and transfer of your personal information. We recommend that you
+read the privacy policies of other websites and online services,
+which you use.</p>
+                            </li>
+                            <li id="5.2">
+                                <p>Safety</p>
+                                <p>We and / or our third-party partners have implemented physical, electronic, and management solutions.
+procedures to try to protect and help prevent unauthorized access
+access, maintain data security and use the information correctly,
+collected through the Platform. We accept and commit to the relevant third parties
+take reasonable precautions to protect our users information
+from loss, misuse, unauthorized disclosure, alteration, and
+destruction.</p>
+                                <p>If you believe that your information is being used by us or by any third party
+if the party is acting improperly, please contact us immediately by stating " Problem
+security" in the subject line.</p>
+                            </li>
+                        </ol>';
+
+$MESS['PP_LIST_ITEM_06'] = '<h2 class="policy-list_title">STORING INFORMATION</h2>
+                        <p>We will store your non-personal information for as long as necessary,
+to provide you with the Platform or otherwise fulfill the purposes described above,
+except where further storage is required to establish, enforce or
+defend a legal claim or to comply with applicable law, including for
+accounting rules.</p>
+                        <p>Your non-personal information is deleted as soon as it no longer serves as one of the
+for the above-mentioned purposes.</p>
+                        <h3>Access of our contractors</h3>
+                        <p>We or our contractors regularly maintain your data. All third parties must
+agree to respect the privacy of our users and protect their privacy
+their information. This means that your data cannot be transferred to other parties, and it is not allowed to
+there should be direct third-party marketing.</p>';
+
+$MESS['PP_LIST_ITEM_07'] = '<h2 class="policy-list_title">CHANGES TO THIS PRIVACY POLICY</h2>
+                        <p>We reserve the right, at our sole discretion, to change, modify or otherwise
+change this Privacy Policy at any time. You can always
+check the latest version of our Privacy Policy on the Website. If you don"t
+If you agree to any changes to this Privacy Policy, you must stop
+using the Platform. Your continued use of the Platform after the publication of any
+changes to this Privacy Policy will mean that you agree to
+revised Privacy Policy.</p>';
+
+$MESS['PP_LIST_ITEM_08'] = '<h2 class="policy-list_title">USER RIGHTS (EU RESIDENTS)</h2>
+                        <p>The EU General Data Protection Regulation (EU GDP) provides the following rights for
+individuals with respect to personal data, in the case of their processing:</p>
+                        <p>1. Right to receive information</p>
+                        <p>2. Access rights</p>
+                        <p>3. Right to rectification</p>
+                        <p>4. Right to erasure</p>
+                        <p>5. Right to restrict processing</p>
+                        <p>6. Right to data portability</p>
+                        <p>7. Right to object</p>
+                        <p>8. Rights in relation to automated decision-making and profiling.</p>
+                        <h3>How to exercise these rights</h3>
+                        <p>Any requests about the use of your personal data can be sent to us via
+contact information specified in this document. These requests can be executed
+free of charge, and we will review them as early as possible, but no later than within one month
+months.</p>
+                        <h3>Childrens online privacy</h3>
+                        <p>Our Platform is not intended for persons under the age of 14. We do not collect any personal data.
+data.</p>';
+
+$MESS['PP_LIST_ITEM_09'] = '<h2 class="policy-list_title">CONTACT US</h2>
+                        <p>KICK GAME ESPORTS LIMITED
+                            <br>
+                            Vasili Michailidi, 21, Limassol, 3026, Cyprus
+                            <br>
+                            e-mail: <a href="mailto:support@kick.game">support@kick.game</a>
+                        </p>';
---
+++ regulations/lang/en/index.php
@@ -0,0 +1,27 @@
+<?
+$MESS['REGULATIONS_TITLE'] = 'Regulations';
+
+$MESS['REGULATIONS_TEXT_P1'] = 'These Tournament Rules (Regulations) from KICKGAMER SPORTS LIMITED govern the organization of the event, its conduct and conditions. These rules apply to all participants.';
+$MESS['REGULATIONS_TEXT_P2'] = 'All decisions regarding the admission of players to participate, the schedule and conduct of tournament events, as well as disciplinary actions in connection with unacceptable behavior are made at the sole discretion of the Organizer of KICKGAME ESPORTS LIMITED. The decisions of the Organizer of KICK GAME SPORTS LIMITED are final and binding, and cannot serve as the basis for any claims for monetary damages or any other legal remedies.';
+$MESS['REGULATIONS_TEXT_P3'] = 'The team captain / manager is responsible for providing the team Members with this information related to these Tournament Rules.';
+$MESS['REGULATIONS_TEXT_P4'] = 'Participation in the tournament means acceptance of all changes to the rules, instructions and guidelines.';
+
+$MESS['REGULATIONS_TITLE_2_1'] = 'Consent to comply with the Competition Rules';
+$MESS['REGULATIONS_ITEM_1_1'] = 'Approval.';
+$MESS['REGULATIONS_ITEM_1_1_TEXT'] = 'Each team Member must accept these Competition Rules in order to participate in the games. A team member can accept these Competition Rules by registering to participate in the KICKGAME ESPORTS LIMITED tournament. 1.2 Changes to the Competition Rules.';
+$MESS['REGULATIONS_ITEM_1_2'] = 'Changes to the tournament rules.';
+$MESS['REGULATIONS_ITEM_1_2'] = 'The competition rules will need to be updated and supplemented periodically, taking into account the development of this area and updates of the game PUBG MOBILE. Therefore, the organizer of the KICK GAME SPORTS LIMITED may periodically update, correct or supplement these terms and conditions. Competition rules. All information about the changes made will be published on the platform <a href="https://kick.game/">https://kick.game</a>';
+
+$MESS['REGULATIONS_TITLE_PLAYERS'] = 'Players';
+$MESS['REGULATIONS_ITEM_PLAYERS_TEXT'] = 'Admission to participate.';
+$MESS['REGULATIONS_ITEM_PLAYERS_TITLE'] = 'Player names';
+$MESS['REGULATIONS_ITEM_PLAYERS_P1'] = 'The players tag or game alias (hereinafter referred to as the «Player Tag») must consist of the Team name (or an abbreviation or other word representing the Team name) and the players name in the format «TEAM NAME PLAYER NAME». For example, if the players name is «Hero» and he is from the «Super» team, his Player tag will be «superHERO». The team tag may differ depending on the teams preferences, but the sequence «TEAM NAMEPLAYNAME» is a prerequisite. The players tag or nickname must not contain any words or phrases in any language that are offensive, threatening or offensive. The KICK GAME organizer reserves the right to reject any player Tag or nickname selected by the player and require the player to select another player Tag or nickname that complies with these Competition Rules.';
+$MESS['REGULATIONS_ITEM_PLAYERS_P2'] = 'Playing on the emulator is prohibited. Players with an account level of at least 30 are allowed to participate in the tournament.';
+$MESS['REGULATIONS_ITEM_PLAYERS_P3'] = 'The list of players to register must consist of players with the SAME team tag, one player with a different clan tag is allowed. Nicknames and tags should be regulated by the rule.';
+$MESS['REGULATIONS_ITEM_PLAYERS_P4'] = 'At the moment of being in the match lobby at any stage of the tournament, 3 players of the team must have the same team tag. The minimum number of players in the lobby is 3 players.';
+$MESS['REGULATIONS_ITEM_PLAYERS_P5'] = 'The player has the right to change the nickname after notifying the Organizers of the tournament.';
+$MESS['REGULATIONS_ITEM_PLAYERS_P6'] = 'Players are prohibited from having avatars that are offensive or sexual in nature. This applies to the avatars of the discord server participants and the participants who are in the lobby.';
+
+$MESS['REGULATIONS_TITLE_REG'] = 'Registration';
+$MESS['REGULATIONS_ITEM_REG_P1'] = 'All players are required to follow the Registration Rules in order to be eligible for the Tournament. All players must register (as a group along with their teammates) to participate in the KICKGAME ESPORTS LIMITED tournament. Players who do not meet all the conditions for registration will not be allowed to participate in the tournament from the organization. A player in our tournaments must only compete for one team. The administration of the organization has the right to refuse to register the team without explaining the reasons. Teams from all regions who speak Russian or English are allowed to register.';
+$MESS['REGULATIONS_ITEM_REG_P2'] = 'Registration takes place by submitting an application on an available date according to the players rating. The list of games and the schedule are regularly published on the website <a href="https://kick.game/">https://kick.game/</a>';
