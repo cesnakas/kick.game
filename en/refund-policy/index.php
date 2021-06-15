afb3742d0d413da4761d52e8922b671627338784
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-06-16 01:46:05 +0400

    updated eng refund-policy & terms-conditions

--- en/refund-policy/index.php
+++ en/refund-policy/index.php
@@ -1,2 +1,79 @@
 <?
-require($_SERVER['DOCUMENT_ROOT'].'/refund-policy/index.php');
+// require($_SERVER['DOCUMENT_ROOT'].'/refund-policy/index.php');
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+echo '<style>.layout .content ul li:before {top:14px;width:8px;height:8px;}</style>';
+?>
+
+    <div class="container">
+
+        <h1 class="text-center">KICK GAME eSports<br>REFUND POLICY</h1>
+
+        <div class="content">
+
+            <div>Last update: «22» March 2021</div>
+            <div>Version 1.0.</div>
+
+            <p>These refund terms (hereinafter referred to as <b>“Rules”</b>) set forth in this Policy govern the contractual relationship between You and KICK GAME ESPORTS LIMITED (registration number HE 416108) - a company registered in accordance with the legislation of the Republic of Cyprus with its registered office at: Vasili Michailidi, 21, Limassol, 3026, Cyprus (hereinafter - <b>"We"</b>, <b>"KICK GAME eSports"</b>, <b>"Company"</b>).</p>
+
+            <div class="policy">
+
+                <ol class="policy-list">
+
+                    <li id="1">
+                        <h2 class="policy-list_title">DEFINITIONS</h2>
+                        <p><b>Website</b> (hereinafter - <b>"Website"</b>, <b>"Platform"</b>) - the site <a href="https://kick.game">kick.game</a>, administered by KICK GAME eSports Ltd, through which the KICK GAME eSports Services are provided, as well as access to information and analytical materials on it in accordance with the terms and conditions specified in the User Agreement, as well as other documents to which this User Agreement refers.</p>
+                        <p><b>KICK GAME eSports Services</b> (hereinafter referred to as the <b>"Services"</b>) – provision to the User of an access to the internal functionality of the Website.</p>
+                    </li>
+
+                    <li id="2">
+                        <h2 class="policy-list_title">DELIVERY</h2>
+                        <p>After you have paid for access to the internal functionality of the Website and/or other services provided by KICK GAME eSports, the purchase information will be immediately displayed in your Personal Account. We will send you a confirmation message about the purchase, which will be placed in the notifications on the Platform.</p>
+                    </li>
+
+                    <li id="3">
+                        <h2 class="policy-list_title">ILLEGAL ACTIONS</h2>
+                        <p>We pay close attention to any fraud, especially in the case of using cheats while using the Platform. KICK GAME eSports reserves the right to terminate access to the Platform and the Services in case of their illegal use or any other actions to which the User does not have the right.</p>
+                        <p>Please note that users whose Accounts are blocked do not have the right to claim a refund, as well as any other types of compensation.</p>
+                    </li>
+
+                    <li id="4">
+                        <h2 class="policy-list_title">GROUNDS FOR A REFUND</h2>
+                        <p>KICK GAME eSports will consider your refund claims for the following reasons:</p>
+                        <ul>
+                            <li><u>Failure to access the Platform or KICK GAME eSports Services.</u> <br>If you paid for access to the Services but did not receive the access within the promised period, please contact our Support team at <a href="mailto:support@kick.game">support@kick.game</a> and specify all the details about the payment. Complaints about the failure to obtain the access must be submitted to the Support team in writing within 72 hours from the date of payment. If access has not been granted, you will be asked to get access at the time of the request, otherwise you will be offered a refund. Please note that if you do not submit a claim within the specified time limit, access will be considered granted.</li>
+                            <li><u>You accidentally made a double payment.</u> <br>In this case, please contact our Support team at <a href="mailto:support@kick.game">support@kick.game</a>. The refund will be made in the case of a duplicate purchase made within 5 minutes after the last transaction for the same Service provided that you have not already started to use the Service and have contacted technical support no later than 7 days after making the purchase.</li>
+                            <li><u>You forgot to cancel your subscription and were billed.</u></li>
+                            <li>A refund will be made for the first renewal provided that you have contacted the Support team at <a href="mailto:support@kick.game">support@kick.game</a> within 24 hours from the date of debiting funds for the subscription and have not used access to the Services that are provided under the subscription.</li>
+                            <li><u>You have changed your mind or decided that you made a mistake with the purchase of access to the Services, and you do not need them.</u> <br>However, if the access has already been activated, implemented or used in any other way, a refund is not possible.</li>
+                            <p>Please note that the following reasons may not be the basis for a refund:</p>
+                            <li><u>The Services are not compatible with the existing configuration of your PC or mobile settings.</u> <br>Please note that we do not bear any responsibility, and do not respond to requests for refunds / compensations / exchanges due to incompatibility of the Services with third-party programs (plugins, add-ons, modules, search engines, scripts, extensions, etc.). We do not guarantee that the Services are fully compatible with any third-party applications, and we do not support third-party applications, so please read the description carefully before purchasing.</li>
+                            <li><u>General statements that do not constitute a specific complaint.</u><br>For example, "It's not what I thought it was," or "I can't use it." If you do not understand some description – just ask our Support team the necessary questions.</li>
+                        </ul>
+                    </li>
+
+                    <li id="5">
+                        <h2 class="policy-list_title">TERMS OF REFUND</h2>
+                        <p>The refund is made using the same payment method that you used when making the purchase. The refund period depends on the refund method and is usually between 2 and 10 days, but it can be extended to 30 days for certain payment methods. Please note that not all payment systems, we cooperate with, support the refund procedure. If the payment system you made the payment does not support refunds, we will contact you to discuss alternative refund methods.</p>
+                    </li>
+
+                    <li id="6">
+                        <h2 class="policy-list_title">DISPUTE RESOLUTION</h2>
+                        <p>KICK GAME eSports represents and warrants that it will take all necessary actions to resolve your problem in good faith. All disputes concerning refund will be handled in accordance with the applicable laws of the Republic of Cyprus.</p>
+                    </li>
+
+                    <li id="7">
+                        <h2 class="policy-list_title">MISCELLANEOUS</h2>
+                        <p>All your refund requests must be accompanied by a detailed explanation of why you are requesting a refund. Please make sure that your request does not contradict the terms of this Policy. If you have any additional questions or problems related to the refund, please contact our Support team for more information.</p>
+                    </li>
+
+                </ol>
+
+            </div>
+
+        </div>
+
+    </div>
+
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
+?>
