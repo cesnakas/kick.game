0467007186db32e049bd9f8c9eb09b2194b29470
Sergei ★ Česnakas <10352420+cesnakas@users.noreply.github.com>
2021-07-08 15:58:14 +0400

    update header js

--- local/templates/main/dist/js/main.js
+++ local/templates/main/dist/js/main.js
@@ -2,7 +2,7 @@
 $.getScript('https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js');
 
 // Header
-if(window.location.pathname === '/') {
+if(window.location.pathname === '/' && ($(window).outerWidth() >= 992)) {
     let navbar = $('.navbar');
     navbar.addClass('navbar-blur');
     $(function() {
